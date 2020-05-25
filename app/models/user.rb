f3b55244ab8fd0c893eae6f1e9819f95801d0a52
Gatsey <felixgate13@gmail.com>
2020-05-25 11:56:19 +0100

    getting the login pages to work how id like and added authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-
+    require 'bcrypt'
     has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:new, :create]
 
   get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
-  get 'authorized', to: 'sessions#page_requires_login'
+  post 'sessions#new', to: 'sessions#create'
+
 
   root 'pages#home', as: 'home'
   get 'pages/my_story', as: 'my_story'
 
-
+  resources :users, only: [:new, :create]
   resources :gallery_posts
   resources :categories
   resources :blogs
